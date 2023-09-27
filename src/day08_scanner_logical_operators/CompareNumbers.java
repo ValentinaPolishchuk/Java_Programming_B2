e80ad0165d33e28fb94bdd85e637021d5b3ef8b1
ValentinaPolishchuk <tinapolishchuk21@gmail.com>
2023-09-27 19:22:35 -0400

    My new commit for day 12

--- src/day08_scanner_logical_operators/CompareNumbers.java
+++ src/day08_scanner_logical_operators/CompareNumbers.java
@@ -1,6 +1,27 @@
 package day08_scanner_logical_operators;
 
-public class CompareNumbers {
-    
+import java.util.Scanner;
 
+public class CompareNumbers {
+
+    /*
+        create a Scanner object
+        ask the user to enter 2 numbers
+        check if the numbers are equal to each other
+
+     */
+
+    public static void main (String [] args) {
+
+        Scanner input = new Scanner (System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = input.nextInt(); // we are getting something/info/data from user and storing ot into a container/variable
+        System.out.print("Enter number 2: ");
+        int num2 = input.nextInt();
+
+        System.out.println("Number are equal? " + (num1 == num2));
+
+
+
+    }
 }
